a50149d85c5cb5399614da54a8e440fc186d852c
Ben Smith <programsam@gmail.com>
2019-06-13 14:48:54 -0400

    A very interesting idea.

--- js/nl.js
+++ js/nl.js
@@ -1,69 +1,68 @@
 
-function pageChange() {
+function pageChange(filename) {
   $("body").scrollTop(0);
+  $("#maincontent").load(filename);
 }
 
-$(document).ready(function()
-{
-  $("#maincontent").load("/aboutada.html");
-  $("body").scrollTop(0);
-
-  //
-  // Handlers for the various menu
-  // options; when clicked, what they do.
-
-  $("a#aboutada").click(function() {
-      $('#maincontent').load("/aboutada.html");
-      $("body").scrollTop(0);
-  });
-
-  $("a#testimonials").click(function() {
-    $('#maincontent').load("/testimonials.html");
-    $("body").scrollTop(0);
-  });
-
-  $("a#services").click(function() {
-    $('#maincontent').load("/services.html");
-    $("body").scrollTop(0);
-  });
-
-  $("a#workshops").click(function() {
-    $('#maincontent').load("/workshops.html");
-    $("body").scrollTop(0);
-  });
-
-  $("a#blog").click(function() {
-    window.open("http://naturallisteninglisteningwithlove.blogspot.com/");
-  });
-
-  //
-  // DROPDOWN
-  // Handlers for the various drop-down
-  // menu options and what they do.
-
-  $("a#nl").click(function() {
-      $('#maincontent').load("/nl.html");
-      $("body").scrollTop(0);
-  });
-
-  $("a#butterfly").click(function() {
-    $('#maincontent').load("/like-a-butterfly.html");
-    $("body").scrollTop(0);
-  });
-
-  $("a#book-list").click(function() {
-    $('#maincontent').load("/book-list.html");
-    $("body").scrollTop(0);
-  });
-
-  $("a#making-connections").click(function() {
-    $('#maincontent').load("/making-connections.html");
-    $("body").scrollTop(0);
-  })
-
-  $("a#links").click(function() {
-    $('#maincontent').load("/links.html");
-    $("body").scrollTop(0);
-  });
+if (location.hash === "aboutada") {
+  pageChange("aboutada.html")
+} else if (location.hash === "testimonials") {
+  pageChange("testimonials.html");
+} else {
+  pageChange("aboutada.html")
+}
+// $(document).ready(function()
+// {
+//   pageChange("/aboutada.html");
+//
+//   //
+//   // Handlers for the various menu
+//   // options; when clicked, what they do.
+//
+//   $("a#aboutada").click(function() {
+//       pageChange("/aboutada.html");
+//   });
+//
+//   $("a#testimonials").click(function() {
+//     pageChange("/testimonials.html");
+//   });
+//
+//   $("a#services").click(function() {
+//     pageChange("/services.html");
+//   });
+//
+//   $("a#workshops").click(function() {
+//     pageChange("/workshops.html");
+//   });
+//
+//   $("a#blog").click(function() {
+//     window.open("http://naturallisteninglisteningwithlove.blogspot.com/");
+//   });
+//
+//   //
+//   // DROPDOWN
+//   // Handlers for the various drop-down
+//   // menu options and what they do.
+//
+//   $("a#nl").click(function() {
+//       pageChange("/nl.html");
+//   });
+//
+//   $("a#butterfly").click(function() {
+//     pageChange("/like-a-butterfly.html");
+//   });
+//
+//   $("a#book-list").click(function() {
+//     pageChange("/book-list.html");
+//   });
+//
+//   $("a#making-connections").click(function() {
+//     $('#maincontent').load("/making-connections.html");
+//     $("body").scrollTop(0);
+//   })
+//
+//   $("a#links").click(function() {
+//     pageChange("/links.html");
+//   });
 
 });
