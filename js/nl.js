32337e0b8a867b91883cfe95b3ca2c56c6b48381
Ben Smith <programsam@gmail.com>
2019-06-13 15:05:52 -0400

    Activate the links.

--- js/nl.js
+++ js/nl.js
@@ -34,56 +34,5 @@ $(document).ready(function()
   $(window).bind('hashchange',function(event){
     syncToHash();
   });
-
-//
-//   //
-//   // Handlers for the various menu
-//   // options; when clicked, what they do.
-//
-//   $("a#aboutada").click(function() {
-//       pageChange("/aboutada.html");
-//   });
-//
-//   $("a#testimonials").click(function() {
-//     pageChange("/testimonials.html");
-//   });
-//
-//   $("a#services").click(function() {
-//     pageChange("/services.html");
-//   });
-//
-//   $("a#workshops").click(function() {
-//     pageChange("/workshops.html");
-//   });
-//
-//   $("a#blog").click(function() {
-//     window.open("http://naturallisteninglisteningwithlove.blogspot.com/");
-//   });
-//
-//   //
-//   // DROPDOWN
-//   // Handlers for the various drop-down
-//   // menu options and what they do.
-//
-//   $("a#nl").click(function() {
-//       pageChange("/nl.html");
-//   });
-//
-//   $("a#butterfly").click(function() {
-//     pageChange("/like-a-butterfly.html");
-//   });
-//
-//   $("a#book-list").click(function() {
-//     pageChange("/book-list.html");
-//   });
-//
-//   $("a#making-connections").click(function() {
-//     $('#maincontent').load("/making-connections.html");
-//     $("body").scrollTop(0);
-//   })
-//
-//   $("a#links").click(function() {
-//     pageChange("/links.html");
-//   });
-
+  
 });
