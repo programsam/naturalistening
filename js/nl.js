cb3013f0a443c775bf2b136a5b46ab9305800f95
Ben Smith <programsam@gmail.com>
2024-08-10 11:09:56 -0400

    Update to latest JS modules. Fix toggles.

--- js/nl.js
+++ js/nl.js
@@ -11,23 +11,34 @@ function pageChange(filename) {
 
 function syncToHash() {
   var hash = location.hash.replace('#','');
+  $('.nav-link').removeClass('active');
+  $('.dropdown-item').removeClass('active');
   if (hash === "aboutada") {
+    $('#aboutada').addClass('active');
     pageChange("aboutada.html")
   } else if (hash === "testimonials") {
+    $('#testimonials').addClass('active');
     pageChange("testimonials.html");
   } else if (hash === "services") {
+    $('#services').addClass('active');
     pageChange("services.html");
   } else if (hash === "workshops") {
+    $('#workshops').addClass('active');
     pageChange("workshops.html");
   } else if (hash === "nl") {
+    $('#nl').addClass('active');
     pageChange("nl.html");
   } else if (hash === "butterfly") {
+    $('#butterfly').addClass('active');
     pageChange("like-a-butterfly.html");
   } else if (hash === "book-list") {
+    $('#book-list').addClass('active');
     pageChange("book-list.html");
   } else if (hash === "links") {
+    $('#links').addClass('active');
     pageChange("links.html");
   } else {
+    $('#aboutada').addClass('active');
     pageChange("aboutada.html")
   }
 }
